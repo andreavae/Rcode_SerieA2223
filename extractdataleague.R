54743189dd651a0a6b1260798fdea3c93ef446c2
Andrea Vaerini <77801782+andreavae@users.noreply.github.com>
2025-03-18 17:07:38 +0100

    Delete extractdataleague.R

--- extractdataleague.R
+++
@@ -1,8 +0,0 @@
-install.packages("worldfootballR")
-library(worldfootballR)
-
-# to get the serie a results:
-seriea_results <- understat_league_match_results(league = "Serie A", season_start_year = 2020)
-ds=dplyr::glimpse(seriea_results)
-
-
